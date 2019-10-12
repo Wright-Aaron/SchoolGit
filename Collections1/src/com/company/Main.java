b98a2ce7d5a1daa216aa2c2411b2cd82b7248b06
Wright-Aaron <wri14006@gmail.com>
2019-10-12 13:36:09 -0500

    Hash set example

--- Collections1/src/com/company/Main.java
+++ Collections1/src/com/company/Main.java
@@ -6,8 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Create a random number generator.
         Random rand = new Random();
 
+        // Create two hash sets and fill them with random numbers. Unlike list there will be no duplicate accepted.
         Set<Integer> setOne = new HashSet();
         for (int i = 0; i <= 15; i++) {
             setOne.add(rand.nextInt(30));
@@ -17,28 +19,29 @@ public class Main {
             setTwo.add(rand.nextInt(30));
         }
 
+        // Display the sets that were created.
         System.out.println("List one: \n" + setOne);
         System.out.println("List two: \n" + setTwo);
 
-        // To find union
+        // Find union of the two sets and print
         Set<Integer> union = new HashSet(setOne);
         union.addAll(setTwo);
         System.out.println("Union of the two Set:");
         System.out.println(union);
 
-        // To find intersection
+        // Find intersection of the two sets and print.
         Set<Integer> intersection = new HashSet(setOne);
         intersection.retainAll(setTwo);
         System.out.println("Intersection of the two Set:");
         System.out.println(intersection);
 
-        // To find the symmetric difference
+        // Find the symmetric difference of the two sets and print.
         Set<Integer> difference = new HashSet(setOne);
         difference.removeAll(setTwo);
         System.out.println("Difference of the two Set:");
         System.out.println(difference);
 
-        // Iterating over hash set items
+        // Iterating over the union set.
         System.out.println("Iterating over list:");
         Iterator<Integer> i = union.iterator();
         while (i.hasNext())

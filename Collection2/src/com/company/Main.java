4daadbd7fc305bdc7e817ae9745a54fb6275241a
Wright-Aaron <wri14006@gmail.com>
2020-02-04 16:54:18 -0600

    Commit

--- Collection2/src/com/company/Main.java
+++ Collection2/src/com/company/Main.java
@@ -9,6 +9,7 @@ public class Main {
         Random rand = new Random();
         Scanner Input = new Scanner(System.in);
         boolean cont = true;
+        String usrSrch;
 
         Map<String,String> Employees = new HashMap<>();
 
@@ -16,21 +17,20 @@ public class Main {
             System.out.println("Enter employee name: (Format: First Last)");
             System.out.println("Type quit when you are done.");
             String empName = Input.nextLine();
-            if (empName == "quit") {
+            if (empName.equals("quit")) {
                 cont = false;
             }
             else {
                 String[] splitName = empName.split(" ");
                 String initials = new StringBuilder().append(splitName[0].charAt(0)).append(splitName[1].charAt(0)).toString();
                 Employees.put(empName, initials + rand.nextInt(1000));
-                System.out.println();
             }
         }
 
         cont = true;
         while (cont) {
             System.out.println("Which employee's EID would you like?");
-            String usrSrch = Input.nextLine();
+            usrSrch = Input.nextLine();
             if (Employees.containsKey(usrSrch)) {
                 System.out.println(Employees.get(usrSrch));
             }
